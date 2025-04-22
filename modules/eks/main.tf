0c136a102fc1e355a2bd70c699a03572aca82bd5
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-22 11:05:33 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,18 @@
+data "aws_eks_cluster" "main" {
+  name = aws_eks_cluster.main.name
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
+}
+
+# Configure Kubernetes provider with data from the created EKS cluster
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = var.eks_cluster_role_arn
