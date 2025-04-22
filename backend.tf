7bd0208007ecd168044b19a6388a6a0edfb51656
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-22 16:23:53 +0530

    updated changes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-aws-bucket-3550"
+    bucket         = "my-new-bucket-355057"
     key            = "goutham/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
